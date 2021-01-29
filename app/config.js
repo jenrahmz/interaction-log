70592a975947a8675bce5c84e4798ac75e10652f
Pete Jobes <petejobes.digitaldwp@gmail.com>
2021-01-29 10:25:50 +0000

    name update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Assessment Service',
+  serviceName: 'Health Assessment Service',
 
   // Default port that prototype runs on
   port: '3000',
