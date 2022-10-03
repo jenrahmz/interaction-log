bfa336fb84c0322c66f2188d31044378727c7b92
jenrahmz <78032800+jenrahmz@users.noreply.github.com>
2022-10-03 14:39:52 +0100

    v0.3.6 compatible with claimant contact

--- app/routes.js
+++ app/routes.js
@@ -370,6 +370,41 @@ router.post('/version-0-3-1/contact-claimant', function (req, res) {
                   })
 
 
+                  router.post('/version-0-3-6/log-condensed', function (req, res) {
+                    // Get the answer from session data
+                    // The name between the quotes is the same as the 'name' attribute on the input elements
+                    // However in JavaScript we can't use hyphens in variable names
+                  
+                    const what = req.session.data['what']
+                  
+                    if (what == 'outgoing') {
+                        res.redirect('/version-0-3-6/outgoing')
+                      } 
+                      else if (what == 'incoming') {
+                        res.redirect('/version-0-3-6/incoming')
+                      }
+                      else {
+                        res.redirect('/version-0-3-6/event')
+                      }
+                    })
+        
+                    router.post('/version-0-3-6/user-contact', function (req, res) {
+                      // Get the answer from session data
+                      // The name between the quotes is the same as the 'name' attribute on the input elements
+                      // However in JavaScript we can't use hyphens in variable names
+                    
+                      const usercontact = req.session.data['user-contact']
+                    
+                      if (usercontact == 'yes') {
+                          res.redirect('/version-0-3-6/outgoing')
+                        } 
+                       
+                        else {
+                          res.redirect('/version-0-3-6/index')
+                        }
+                      })
+
+
         router.post('/version-0-3-2/edit-page', function (req, res) {
           // Get the answer from session data
           // The name between the quotes is the same as the 'name' attribute on the input elements
